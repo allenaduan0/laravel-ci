0053053c3f8ee91bd2096baecb83b896a2ede98f
Allen Christopher <devops.allen.aduan@gmail.com>
2026-01-13 15:44:38 +0400

    Setup in-memory SQLite DB for CI tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run migrations
+        $this->artisan('migrate')->run();
+
+        // Optional: seed test data if needed
+        // $this->artisan('db:seed')->run();
+    }
 }
